9f0e0c546f770e52b4468767be33b9411f166f68
jygj65536 <41312810+jygj65536@users.noreply.github.com>
2019-02-13 21:32:09 +0900

    Update picnic.py

--- picnic.py
+++ picnic.py
@@ -1,11 +1,11 @@
 def makepair(listToPair):
     global ans
     global relation
+    
     if len(listToPair) == 2:
         if relation[listToPair[0]][listToPair[1]]:
             ans += 1
         return
-
     else:
         for i in range(len(listToPair)):
             if(relation[listToPair[0]][listToPair[i]]):
@@ -19,15 +19,12 @@ C = int(input())
 for testcase in range(C):
     ans = 0;
     relation = [[0] * 10 for i in range(10)]
-
+    
     [n, m] = list(map(int, input().split()))
-
     friendshipList = list(map(int, input().split()))
-
     for group in (friendshipList[pos:pos + 2] for pos in range(0, len(friendshipList), 2)):
         relation[group[0]][group[1]] = 1
         relation[group[1]][group[0]] = 1
-
+    
     makepair([i for i in range(n)])
-
     print(ans)
